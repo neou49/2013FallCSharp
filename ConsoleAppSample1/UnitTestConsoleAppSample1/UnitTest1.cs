62f93687133705bb83c883afb6c795e834b64494
neou49 <neou49@hawkmail.newpaltz.edu>
2013-09-09 16:38:19 -0400

    Commit first test

---
+++ ConsoleAppSample1/UnitTestConsoleAppSample1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestConsoleAppSample1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int num = 1 + 1;
+            Assert.AreEqual(2, num);
+
+            // Not able to test Main() yet
+            //ConsoleAppSample1.Program.Main(null);
+
+            var json = ConsoleAppSample1.Program.GetProfile("jewpaltz");
+            Assert.IsTrue(json.Contains("Moshe"));
+        }
+    }
+}
