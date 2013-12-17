5f1af784f60d95d521dfba56e22a9e331e735a5f
neou49 <neou49@hawkmail.newpaltz.edu>
2013-12-16 19:04:13 -0500

    WpfAutoCompleteTextboxInClassFinal

---
+++ WpfAutoCompleteTextboxInClassFinal/WpfAutoCompleteTextboxInClassFinal/AutoCompleteTextboxModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAutoCompleteTextboxInClassFinal
+{
+    public class AutoCompleteTextboxModel : BaseViewModel
+    {
+
+        public AutoCompleteTextboxModel()
+        {
+            this._cityList = new List<string>();
+        }
+
+        private List<string> _cityList;
+        public List<string> CityList
+        {
+            get { return _cityList; }
+            set
+            {
+                _cityList = value;
+                NotifyPropertyChanged("CityList");
+            }
+        }
+
+        private string _selectedCity;
+        public string SelectedCity
+        {
+            get { return _selectedCity; }
+            set
+            {
+                _selectedCity = value;
+                NotifyPropertyChanged("SelectedCity");
+            }
+        }
+    }
+
+}
---
+++ WpfAutoCompleteTextboxInClassFinal/WpfAutoCompleteTextboxInClassFinal/AutoCompleteTextboxViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+using System.Web.Services.Description;
+
+namespace WpfAutoCompleteTextboxInClassFinal
+{
+
+    public class AutoCompleteTextboxViewModel: AutoCompleteTextboxModel
+    {
+
+        #region "Constructor"
+        public AutoCompleteTextboxViewModel()
+        {
+            Search();
+        }
+        #endregion
+
+        public async void Search()
+        {
+            string uri = "http://cs.newpaltz.edu/~plotkinm/2012Grad/Final/api/Cities.php";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+
+            string returnString = await response.Content.ReadAsStringAsync();            
+
+            this.CityList.Add(returnString);
+            //string responseBody = await response.Content.ReadAsStringAsync();         
+            
+        }
+
+    }
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+    }
+}
